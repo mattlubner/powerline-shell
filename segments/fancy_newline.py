90641325d69d23dfa1e60b140e7af9387c16ce6b
Matt Lubner <matt@mattlubner.com>
2015-07-19 18:28:01 -0700

    more tweaking

--- segments/fancy_newline.py
+++ segments/fancy_newline.py
@@ -2,16 +2,18 @@ def add_fancy_newline_segment():
     try:
         fg = Color.FANCY_NEWLINE_FG
         bg = Color.FANCY_NEWLINE_BG
-        bg2 = Color.FANCY_NEWLINE_BG2
-        sc = Color.FANCY_NEWLINE_SC
     except:
         fg = Color.HOSTNAME_FG
         bg = Color.HOSTNAME_BG
-        bg2 = Color.HOSTNAME_BG
-        sc = Color.HOSTNAME_BG
 
-    powerline.append(u'  \u21A9  ', fg, bg)
-    powerline.append('', None, bg2, None, sc)
+    try:
+        tail_fg = Color.FANCY_NEWLINE_TAIL_FG
+        tail_fg_adj = Color.FANCY_NEWLINE_TAIL_FG_OVER_RESET
+        powerline.append(u'  \u21A9  ', fg, bg)
+        powerline.append('', None, tail_fg_adj, None, tail_fg)
+    except:
+        powerline.append(u'  \u21A9  ', fg, bg, '')
+
     powerline.append('\n', None, 'reset', '')
 
 add_fancy_newline_segment()
